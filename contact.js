196927c9006d56254fc9b3e55c978fda73c81fe0
tharikmohomed118-crypto <tharikmohomed118@gmail.com>
2025-09-22 13:06:55 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  alert('Thank you for contacting us. We will get back to you soon!');
-  
-  
-  document.getElementById('contact-form').reset();
-});
